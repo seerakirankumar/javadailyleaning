271081e96f38d9c236992ca3112ec2b37dacf4e3
seerakirankumar <kiranseera2007@gmail.com>
2026-03-14 01:24:01 +0530

    todays advance pattern printing questions practice

--- javadailylearning/Pattern.java
+++ javadailylearning/Pattern.java
@@ -132,54 +132,108 @@ public class Pattern {
 
     // }
     // System.out.println();
+    // // }
+
+    // // printing the pattern(butterfly pattern)
+
+    // int n = 5;
+    // for (int i = 1; i <= n; i++) {
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("*");
+    // }
+    // int space = 2 * (n - i);
+    // for (int j = 1; j <= space; j++) {
+    // System.out.print(" ");
+
+    // }
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("*");
+    // }
+    // System.out.println();
+
+    // }
+    // for (int i = n; i >= 1; i--) {
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("*");
+    // }
+    // int space = 2 * (n - i);
+    // for (int j = 1; j <= space; j++) {
+    // System.out.print(" ");
+
+    // }
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("*");
+    // }
+    // System.out.println();
+
     // }
 
-    // printing the pattern(butterfly pattern)
+    // // printin the pattern n=5(solid rhombus)
+
+    // int x = 5;
+    // for (int i = 1; i <= x; i++) {
+    // for (int j = 1; j <=x - i; j++) {//for printing the spaces
+    // System.out.print(" ");
+    // }
+    // for (int j = 1; j <= x; j++) {
+    // System.out.print("* ");
+    // }
+    // System.out.println();
+    // }
+
+    // }
+    // }
+
+    // printing the pattern(number pyramid)
 
     int n = 5;
     for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= i; j++) {
-        System.out.print("*");
-      }
-      int space = 2 * (n - i);
-      for (int j = 1; j <= space; j++) {
-        System.out.print(" ");
-
-      }
-      for (int j = 1; j <= i; j++) {
-        System.out.print("*");
-      }
-      System.out.println();
-
-    }
-    for (int i = n; i >= 1; i--) {
-      for (int j = 1; j <= i; j++) {
-        System.out.print("*");
-      }
-      int space = 2 * (n - i);
-      for (int j = 1; j <= space; j++) {
-        System.out.print(" ");
-
-      }
-      for (int j = 1; j <= i; j++) {
-        System.out.print("*");
-      }
-      System.out.println();
-
-    }
-
-    // printin the pattern n=5(solid rhombus)
-
-    int x = 5;
-    for (int i = 1; i <= x; i++) {
-      for (int j = 1; j <=x - i; j++) {//for printing the spaces
+      for (int j = 1; j <= n - i; j++) {
         System.out.print(" ");
       }
-      for (int j = 1; j <= x; j++) {
-        System.out.print("* ");
+
+      for (int j = 1; j <= i; j++) {
+        System.out.print(i + " ");
       }
       System.out.println();
     }
 
+    // print the pattern(palindromic pattern)
+
+    int m = 6;
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= m - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = i; j >= 1; j--) {
+        System.out.print(j);
+      }
+      for (int j = 2; j <= i; j++) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+
+    // printing the pattern(diamond pattern)
+
+    int k = 4;
+    for (int i = 1; i <= k; i++) {
+      for (int j = 1; j <= k - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for (int i = k; i >= 1; i--) {
+      for (int j = 1; j <= k - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
