77a3cec83ad0bcb703f506520bd50469885d0a37
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-03-25 23:04:02 +0530

    distance calculator question practiced

--- javadailylearning/bytexlpq.java
+++ javadailylearning/bytexlpq.java
@@ -59,14 +59,25 @@ public class bytexlpq {
       
     
   // COMPUTING BMI
-  double weight=sc.nextDouble();
-    double height=sc.nextDouble();
+  // double weight=sc.nextDouble();
+  //   double height=sc.nextDouble();
      
-    double bmi =(weight*703)/(height*height);
-    System.out.printf("Your BMI is: %.2f",bmi);
-    sc.close();
-  }
-}
+  //   double bmi =(weight*703)/(height*height);
+  //   System.out.printf("Your BMI is: %.2f",bmi);
+  //   sc.close();
+  
 
-    
+
+// distance calculator
+ double distance = sc.nextDouble();
+        double time = sc.nextDouble();    
+
+        double speedMps = distance / time;
+        double speedKmph = speedMps * 3.6;
+        double speedMph = speedMps * 2.23694;
+        System.out.printf("Speed: %.2f meters/second (m/s)\n",speedMps);
+        System.out.printf("Speed: %.2f kilometers/hour (km/h)\n",speedKmph);
+        System.out.printf("Speed: %.2f miles/hour (mph)\n",speedMph);
+    }
+}
         
